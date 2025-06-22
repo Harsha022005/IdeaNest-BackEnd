82c153ebc2270039b61457f8766affab00001e16
Your Full Name <harshajami2005@gmail.com>
2025-06-22 14:39:14 +0530

    changed

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ import Chatroutes from './Routes/chatinbox.js';
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: `${process.env.FRONTEND_URL}`, credentials: true }));
-
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 
 app.use(session({
